65bc218be616a481ccc20bb2b8c48a00f6f68334
Naimur Rahman <rahmannayannaimur@gmail.com>
2025-09-01 01:50:04 +0600

    completed

---
+++ Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartHRIS.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+
+        [Required, StringLength(100)]
+        public string Name { get; set; } = default!;
+
+        [Required, EmailAddress, StringLength(100)]
+        public string Email { get; set; } = default!;
+
+        [Required, StringLength(15)]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; } = default!;
+
+        [StringLength(50)]
+        public string? City { get; set; }
+
+        [StringLength(50)]
+        public string? Country { get; set; }
+
+        [Display(Name = "Zip Code"), StringLength(10)]
+        public string? ZipCode { get; set; }
+    }
+}
